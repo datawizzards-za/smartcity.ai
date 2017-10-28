d91f4baea937f098eef354e3664fd676a1028f67
Ofentswe1 <ofentswel@gmail.com>
2017-10-28 15:00:46 -0700

    Models added to the following

--- app/models.py
+++ app/models.py
@@ -2,5 +2,43 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Employee(models.Model):
+    emp_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    job_title = models.CharField(max_length=150)
+    specialization = models.CharField(max_length=50)
+    job_desc = models.TextField(max_length=1000)
+    cell = models.CharField(max_length=20)
+
+class Citizen(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    cell = models.CharField(max_length=20)
+
+class Faults(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(max_length=1000)
+    reporter = models.ForeignKey(Citizen, on_delete=models.CASCADE)
+    location = models.CharField(max_length=150)
+
+class TaskManager(models.Model):
+    fault_id = models.ForeignKey(Faults, on_delete=models.CASCADE)
+    responder = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10)
+    reason = models.CharField(max_length=150)
+
+class Recommended(models.Model):
+    emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+class Schedular(models.Model):
+    fault_id = models.ForeignKey(Faults, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20)
+    Recommended = models.ForeignKey(Recommended, on_delete=models.CASCADE)
+
+class Address(models.Model):
+    line_one = models.CharField(max_length=100)
+    line_two = models.CharField(max_length=100)
+    gps = models.CharField(max_length=60)
+    city = models.CharField(max_length=15)
+    province = models.CharField(max_length=15)
