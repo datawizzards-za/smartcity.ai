76e259292fc2eafe1a922226c65e3bbc50b8c4a9
Mabu <manailengmj@gmail.com>
2017-10-29 20:09:50 +0200

    views

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^\Z$', views.TaskMan.as_view(), name='home'),
     url(r'^task-man/$', views.TaskMan.as_view(), name='task-man'),
     url(r'^vacancies/$', views.Vacancies.as_view(), name='vacancies'),
-    url(r'^load_employees_data/$', views.LoadEmployeesData.as_view(), 
+    url(r'^load_employees_data/$', views.LoadEmployeesData.as_view(),
         name='load_employees_data'),
     url(r'^address/(?P<username>\w+)/', api_views.GetClientAddress.as_view(),
         name='address'),
@@ -18,11 +18,13 @@ urlpatterns = [
         name='taskmanager'),
 
     url(r'^employee/', api_views.GetEmployees.as_view(), name='employee'),
-    url(r'^personal/(?P<username>\w+)/', api_views.GetUser.as_view(),
-        name='personal'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html',
-'form_class': forms.LoginForm},
-        name='login'),
+
+    url(r'^personal/(?P<username>\w+)/',
+        api_views.GetUser.as_view(), name='personal'),
+
+    url(r'^login/$', auth_views.login,
+        {'template_name': 'login.html', 'form_class': forms.LoginForm}, name='login'),
+
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^register/$', views.RegisterUserView.as_view(), name='register'),
 ]
