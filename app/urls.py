538405096a91cf0311c6cf2a53fb4eb9bc967712
Ofentswe1 <ofentswel@gmail.com>
2017-10-29 17:16:45 +0200

    the login and reegistration

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from app import views
 from app import api_views
-
+from django.contrib.auth import views as auth_views
+from app import forms
 urlpatterns = [
+
     url(r'^\Z$', views.TaskMan.as_view(), name='home'),
     url(r'^task-man/$', views.TaskMan.as_view(), name='task-man'),
     url(r'^vacancies/$', views.Vacancies.as_view(), name='vacancies'),
@@ -16,5 +18,10 @@ urlpatterns = [
     url(r'^employee/', api_views.GetEmployees.as_view(), name='employee'),
     url(r'^personal/(?P<username>\w+)/', api_views.GetUser.as_view(),
         name='personal'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html',
+'form_class': forms.LoginForm},
+        name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^register/$', views.RegisterUserView.as_view(), name='register'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -4,6 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views import View
 from . import models
+from app.forms import RegisterUserForm
+from django.urls import reverse_lazy
+from django.http import HttpResponseForbidden, HttpResponse
+from django.views.generic import CreateView, TemplateView
 # Create your views here.
 
 
@@ -23,7 +27,7 @@ class Vacancies(View):
         return render(request, self.template_name, context)
 
 class Schedular(View):
-    def get(self, requests, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         models.Faults.objects.create(
         name=kwargs['name'],
         description = kwargs['desc'],
@@ -34,3 +38,19 @@ class Schedular(View):
         #context = {''}
         return render(request, self.template_name)
 
+
+class RegisterUserView(CreateView):
+    form_class = RegisterUserForm
+    template_name = "register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return HttpResponseForbidden()
+
+        return super(RegisterUserView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.set_password(form.cleaned_data['password'])
+        user.save()
+        return HttpResponse('User registered')
