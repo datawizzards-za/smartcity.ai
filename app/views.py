a9b4d750ad3b7326b3aff00d4db534a826dea67f
Ofentswe1 <ofentswel@gmail.com>
2017-10-28 18:20:06 -0700

    insert the faults

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views import View
-
+from . import models
 # Create your views here.
 
 
@@ -15,7 +15,14 @@ class EmployeeInbox(View):
         return render(request, self.template_name, context)
 
 class Schedular(View):
-    def get(self, requests):
-        return
+    def get(self, requests, *args, **kwargs):
+        models.Faults.objects.create(
+        name=kwargs['name'],
+        description = kwargs['desc'],
+        reporter = kwargs['user'],
+        location = kwargs['loc'],
+        )
+
         #context = {''}
         return render(request, self.template_name)
+
