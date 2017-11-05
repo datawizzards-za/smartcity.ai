4c6cb237c25b848e069b314519d33c6a56aaeefb
Mabu <manailengmj@gmail.com>
2017-11-05 09:13:29 +0200

    fix(templates): loadstatic was missing in the file.

--- app/static/scripts/caseman.js
+++ app/static/scripts/caseman.js
@@ -9,25 +9,25 @@ $(document).ready(function () {
             mycases = data;
         }
     });
-    
+
     $('#parent_case div#all_case_view').attr("class", "email-content animated rotateInDownRight");
 
-        $('#parent_case ul#case_list').empty();
-        for (i = 0; i < mycases.length; i++) {
-            $('#parent_case ul#case_list').append(addLi(mycases[i].fault));
-        }
+    $('#parent_case ul#case_list').empty();
+    for (i = 0; i < mycases.length; i++) {
+        $('#parent_case ul#case_list').append(addLi(mycases[i].fault));
+    }
 
 
-   addCases('all');
-   caseDetails(mycases[0]);
+    addCases('all');
+    caseDetails(mycases[0]);
 
-   function caseDetails(mycase) {
-       $("#parent_case strong#case-location").html(mycase.fault.location);
-       var status = mycase.status;
-       status = status[0].toUpperCase() + status.slice(1);
-       var desc ="<strong>"+ status + "</strong>  "+ mycase.fault.category +" "+ mycase.fault.defect;
-       $("#parent_case h3#case-desc").html(desc);
-   }
+    function caseDetails(mycase) {
+        $("#parent_case strong#case-location").html(mycase.fault.location);
+        var status = mycase.status;
+        status = status[0].toUpperCase() + status.slice(1);
+        var desc = "<strong>" + status + "</strong>  " + mycase.fault.category + " " + mycase.fault.defect;
+        $("#parent_case h3#case-desc").html(desc);
+    }
 
     $('#parent_case ul#case_nav').on('click', 'li#all_cases', function () {
         $('#new_case_view').hide();
@@ -36,12 +36,12 @@ $(document).ready(function () {
         $('#closed_case_view').hide();
         $('#rejected_case_view').hide();
         $('#pending_case_view').hide();
-        
+
         $('#parent_case div#all_case_view').attr("class", "email-content animated rotateInDownRight");
         $('#all_case_view').show();
 
         //Add Cases
-       addCases('all');
+        addCases('all');
     });
 
     $('#parent_case ul#case_nav').on('click', 'li#new_case', function () {
@@ -51,21 +51,12 @@ $(document).ready(function () {
         $('#inprogress_case_view').hide();
         $('#closed_case_view').hide();
         $('#rejected_case_view').hide();
-        
+
         $('#parent_case div#new_case_view').attr("class", "email-content animated rotateInDownRight");
         $('#new_case_view').show();
 
-<<<<<<< HEAD
-        $('#parent_case ul#case_list').empty();
-        for (i = 0; i < mycases.length; i++) {
-            if (mycases[i].status == 'open') {
-                $('#parent_case ul#case_list').append(addLi(mycases[i].fault));
-            }
-        }
-=======
         //Add Cases
         addCases('open');
->>>>>>> 626e81ad9662144ef00f1a314f3736105f4c0284
     });
 
     $('#parent_case ul#case_nav').on('click', 'li#pending_case', function () {
@@ -75,7 +66,7 @@ $(document).ready(function () {
         $('#closed_case_view').hide();
         $('#rejected_case_view').hide();
         $('#all_case_view').hide();
-        
+
         $('#parent_case div#pending_case_view').attr("class", "email-content animated rotateInDownRight");
         $('#pending_case_view').show();
 
@@ -96,8 +87,8 @@ $(document).ready(function () {
         $('#messaging_view').hide();
         $('#rejected_case_view').hide();
         $('#all_case_view').hide();
-        
-        
+
+
         $('#parent_case div#closed_case_view').attr("class", "email-content animated rotateInDownRight");
         $('#closed_case_view').show();
 
@@ -116,20 +107,15 @@ $(document).ready(function () {
         }
     }
 
-<<<<<<< HEAD
-    function addLi(fault) {
-        return '<li class="hvr-grow"> <a href="javascript:;">' +
-=======
     function addLi(fault, index) {
-        return '<li class="active" value=' + index + '> <a href="javascript:;">' +
->>>>>>> 626e81ad9662144ef00f1a314f3736105f4c0284
+        return '<li value=' + index + '> <a href="javascript:;">' +
             '<div class="group clearfix small">' +
             '<span class="sender-name left text-bold">' + fault.category + '</span>' +
             '<span class="email-date right xsmall mt1 text-pink">' + fault.date_submitted.slice(0, 10)
             + '</span>' +
             '</div >' +
-            '<p class="subject">'+ fault.defect +'</p>' +
-            '<p class="summary small text-muted">' + fault.location+ '</p>' +
+            '<p class="subject">' + fault.defect + '</p>' +
+            '<p class="summary small text-muted">' + fault.location + '</p>' +
             '<div class="group clearfix">' +
             '<span class="ion ion-trash-a left remove-email"></span>' +
             '<span class="ion right ion-paperclip"></span>' +
